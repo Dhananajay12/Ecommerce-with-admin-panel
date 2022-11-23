1fc3876901bc0e4c15be50488eb6b3c9d71ee9eb
Dhananjay12 <dhananjayavare786@gmail.com>
2022-11-23 13:11:59 +0530

    commit search page and pagination

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -1,6 +1,7 @@
 const Product = require("../model/ProductSchema");
 const ErrorHandler = require("../utils/ErrorHandle");
 const asyncHandler = require("express-async-handler");
+const Features = require("../utils/Features");
 
 const createProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -10,21 +11,30 @@ const createProduct = asyncHandler(async (req, res, next) => {
   });
 });
 
-const getAllProducts = asyncHandler(async (req, res) => {
-  const keyword = req.query.search
-    ? {
-        $or: [
-          { name: { $regex: req.query.search, $options: "i" } },
-          { description: { $regex: req.query.search, $options: "i" } },
-        ],
-      }
-    : {};
-
-  const product = await Product.find(keyword);
+const getAdminProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
 
   res.status(200).json({
     success: true,
-    product,
+    products,
+  });
+});
+
+const getAllProducts = asyncHandler(async (req, res) => {
+  const resultPerPage = 8;
+
+  const productsCount = await Product.countDocuments();
+
+  const feature = new Features(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await feature.query;
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
   });
 });
 
@@ -67,6 +77,7 @@ const getSingleProduct = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+    productsCount,
   });
 });
 
